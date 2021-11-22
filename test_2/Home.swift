e492b15ac7c5bc5e85dfea460d37a20effdf47d7
mumu <617958070@qq.com>
2021-11-22 19:28:12 +0800

    add: home

--- test_2/Home.swift
+++ test_2/Home.swift
@@ -8,14 +8,23 @@
 import SwiftUI
 
 struct Home: View {
+    
+    @State var showProfile: Bool = false
+    
     var body: some View {
         ZStack {
+            
+            Color.gray.opacity(0.6)
+                .edgesIgnoringSafeArea(.all)
+            
             VStack {
                 HStack {
                     Text("Welocome")
                         .font(.system(size: 28, weight: .bold, design: .default))
                     Spacer()
-                    Button(action: {}) {
+                    Button(action: {
+                        showProfile.toggle()
+                    }) {
                         Image("avatar")
                             .renderingMode(.original)
                             .resizable()
@@ -28,8 +37,20 @@ struct Home: View {
                 
                 Spacer()
             }
+            .padding(.top, 44)
+            .background(Color.white)
+            //圆角
+            .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
+            .shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 20)
+            .offset(y: showProfile ? -450 : 0)
+            .rotation3DEffect(Angle(degrees: showProfile ? -10 : 0), axis: (x: 1, y: 0, z: 0))
+            .scaleEffect(showProfile ? 0.9 : 1)
+            .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
+            .edgesIgnoringSafeArea(.all)
             
             MenuView()
+                .offset(y: showProfile ? 0 : 600)
+                .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
         }
     }
 }
