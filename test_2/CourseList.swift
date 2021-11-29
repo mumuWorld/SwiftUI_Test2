6b51e945c5473ba70f2c5cad3b4e36cd090ddd8c
mumu <617958070@qq.com>
2021-11-29 17:26:05 +0800

    add: 稍微复杂点的动画

--- test_2/CourseList.swift
+++ test_2/CourseList.swift
@@ -27,44 +27,79 @@ struct CourseView: View {
     @State var show: Bool = false
     
     var section: Section  = Section(title: "Section0", text: "Section0Section0Section0Section0Section0", imgStr: "Illustration1", logoStr: "Logo", color: Color.green.opacity(0.7))
+    
     var body: some View {
-        VStack {
-            HStack(alignment: .top, spacing: 0) {
-                VStack(alignment: .leading) {
-                    Text(section.title)
-                        .font(.system(size: 28, weight: .bold, design: .default))
-                        .frame(width: 160, alignment: .leading)
-                    Text(section.text)
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                }
-                Spacer()
-                Image(section.logoStr)
+        ZStack(alignment: .top) {
+            VStack {
+                Text("The stiffness of the spring, defined as an approximate duration in seconds. A value of zero requests an infinitely-stiff spring, suitable for driving interactive animations")
+                
+                Text("response")
+                    .font(.title).bold()
+                
+                Text("The stiffness of the spring, defined as an approximate duration in seconds. A value of zero requests an infinitely-stiff spring, suitable for driving interactive animation The amount of drag applied to the value being animated, as a fraction of an estimate of amount needed to produce critical damping. The duration in seconds over which to interpolate changes to the response value of the spring The duration in seconds over which to interpolate changes to the response value of the springThe duration in seconds over which to interpolate changes to the response value of the springThe duration in seconds over which to interpolate changes to the response value of the spring The stiffness of the spring, defined as an approximate duration in seconds. A value of zero requests an infinitely-stiff spring, suitable for driving interactive animations")
+                
+                Text("The duration in seconds over which to interpolate changes to the response value of the spring The duration in seconds over which to interpolate changes to the response value of the spring The duration in seconds over which to interpolate changes to the response value of the spring ")
             }
-            .padding(.top)
-            Spacer()
-            Image(section.imgStr)
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 210)
+            .padding(30)
+            .frame(
+                maxWidth: show ? .infinity : screen.width - 60,
+                maxHeight: show ? .infinity : 280,
+                alignment: .top
+            )
+            .offset(y: show ? 460 : 0)
+            .background(Color.white)
+            .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
+            .shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 20)
+            .opacity(show ? 1 : 0)
+            
+            VStack {
+                HStack(alignment: .top, spacing: 0) {
+                    VStack(alignment: .leading) {
+                        Text(section.title)
+                            .font(.system(size: 28, weight: .bold, design: .default))
+                            .frame(width: 160, alignment: .leading)
+                        Text(section.text)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                    }
+                    Spacer()
+                    ZStack {
+                        Image(section.logoStr)
+                            .opacity(show ? 0 : 1)
+                        
+                        Image(systemName: "xmark")
+                            .font(.system(size: 16, weight: .bold, design: .default))
+                            .foregroundColor(.white)
+                            .opacity(show ? 1 : 0)
+                    }
+                    .frame(width: 36, height: 36, alignment: .center)
+                    .background(Color.black)
+                    .clipShape(Circle())
+                }
+                .padding(.top)
+                Spacer()
+                Image(section.imgStr)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 210)
+            }
+            .padding(.horizontal, 20)
+            .padding(.top, show ? 30 : 0)
+            .frame(
+                maxWidth: show ? .infinity : screen.width - 60,
+                maxHeight: show ? 480 : 280
+            )
+            //        .frame(width: screen.width - 60, height: 275, alignment: .center)
+            .background(section.color)
+            .cornerRadius(30)
+            .shadow(color: section.color.opacity(0.3), radius: 20, x: 0, y: 20)
+            //        .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
+            //        .animation(.interactiveSpring())
+            //        .animation(.spring())
+            .onTapGesture {
+                self.show.toggle()
+            }
         }
-        
-        .padding(.horizontal, 20)
-        .padding(.top, show ? 30 : 0)
-        .frame(
-            maxWidth: show ? .infinity : screen.width - 60,
-            maxHeight: show ? .infinity : 280
-        )
-//        .frame(width: screen.width - 60, height: 275, alignment: .center)
-        .background(section.color)
-        .cornerRadius(30)
-        .shadow(color: section.color.opacity(0.3), radius: 20, x: 0, y: 20)
-//        .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
-//        .animation(.interactiveSpring())
-//        .animation(.spring())
         .animation(.spring(response: 0.3, dampingFraction: 0.6, blendDuration: 1))
-        .onTapGesture {
-            self.show.toggle()
-        }
         .edgesIgnoringSafeArea(.all)
     }
 }
